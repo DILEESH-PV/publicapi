7d3ae6b4ac3ad75d48e63e07690787d34ca06000
DILEESH-PV <dileeshpv1998@gmail.com>
2022-11-04 10:41:46 +0530

    %s inserted

--- papi.py
+++ papi.py
@@ -5,17 +5,22 @@ import mysql.connector
 try:
     mydb=mysql.connector.connect(host='localhost',user='root',password='',database='papidb')
 except mysql.connector.Error as e:
-    print("db connection error")
+    print("db connection error",e)
 mycursor=mydb.cursor()
 data=requests.get("https://api.publicapis.org/entries").text
 data_info=json.loads(data)
 for j in data_info['entries']:
     http=str(j['HTTPS'])
-    sql='INSERT INTO `papi`(`api`, `description`, `auth`, `https`, `cors`, `link`, `category`) VALUES ("'+j['API']+'","'+j['Description']+'","'+j['Auth']+'","'+http+'","'+j['Cors']+'","'+j['Link']+'","'+j['Category']+'")'
-    #sql="INSERT INTO `papi`(`api`, `description`, `auth`, `https`, `cors`, `link`, `category`) VALUES ('"+j['API']+"','"+j['Description']+"','"+j['Auth']+"','"+http+"','"+j['Cors']+"','"+j['Link']+"','"+j['Category']+"')"
-    mycursor.execute(sql)
-    mydb.commit()
-    print("Data inserted successfully",j['API'])
+    try:
+        #sql='INSERT INTO `papi`(`api`, `description`, `auth`, `https`, `cors`, `link`, `category`) VALUES ("'+j['API']+'","'+j['Description']+'","'+j['Auth']+'","'+http+'","'+j['Cors']+'","'+j['Link']+'","'+j['Category']+'")'
+        #sql="INSERT INTO `papi`(`api`, `description`, `auth`, `https`, `cors`, `link`, `category`) VALUES ('"+j['API']+"','"+j['Description']+"','"+j['Auth']+"','"+http+"','"+j['Cors']+"','"+j['Link']+"','"+j['Category']+"')"
+        sql="INSERT INTO `papi`(`api`, `description`, `auth`, `https`, `cors`, `link`, `category`) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+        data=(j['API'],j['Description'],j['Auth'],http,j['Cors'],j['Link'],j['Category'])
+        mycursor.execute(sql,data)
+        mydb.commit()
+        print("Data inserted successfully",j['API'])
+    except mysql.connector.Error as e:
+        print("error is",e)
     
 
         
